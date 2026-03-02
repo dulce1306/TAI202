f2d3f9bd7a9d69b1a20924ba9b6307f44ec4eaea
dulce1306 <123046615@upq.edu.mx>
2026-03-02 15:32:01 -0600

    inicio de practica 6

--- miAPI copy/app/main.py
+++ miAPI copy/app/main.py
@@ -26,6 +26,11 @@ class crear_usuario(BaseModel):
     nombre: str = Field(..., min_length=3, max_length=50, example="Juanita")
     edad: int = Field(..., ge=1, le=123, description="Edad valida entre 1 y 123")
 
+# ==========================================
+# Seguridad HTTP BASIC
+# ==========================================
+
+seciurity = HTTPBasic()
 
 # ==========================================
 # ENDPOINTS DE CONSULTA
